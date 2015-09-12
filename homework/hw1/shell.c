ca6089eab776c98c0d7f01da33c8f4ec6cd1a83e
Raynxxx <rayn1027@outlook.com>
2015-09-12 12:03:50 +0800

    Add io redirection for shell.

--- homework/hw1/shell.c
+++ homework/hw1/shell.c
@@ -10,8 +10,9 @@
 #include <sys/wait.h>
 #include <termios.h>
 #include <unistd.h>
-#include <paths.h>
 #include <dirent.h>
+#include <fcntl.h>
+#include <time.h>
 
 #include "io.h"
 #include "parse.h"
@@ -34,8 +35,10 @@ int cmd_quit(tok_t arg[]);
 int cmd_help(tok_t arg[]);
 int cmd_pwd(tok_t arg[]);
 int cmd_cd(tok_t arg[]);
-char* find_file_from_path(char* filename);
-void exec_process(tok_t arg[]);
+char* get_current_time();
+char* find_file_from_path(char* filename, tok_t path_tokens[]);
+int io_redirect(tok_t arg[]);
+void path_resolve(tok_t arg[], tok_t path_tokens[]);
 
 /* Built-in command functions take token array (see parse.h) and return int */
 typedef int cmd_fun_t(tok_t args[]);
@@ -95,38 +98,95 @@ int cmd_cd(tok_t arg[]) {
   return 0;
 }
 
+
+/**
+ * Get current time
+ */
+char* get_current_time() {
+    static char time_buf[64];
+    time_t raw_time;
+    struct tm* cur_time;
+
+    time(&raw_time);
+    cur_time = localtime(&raw_time);
+    strftime (time_buf, sizeof(time_buf), "%H:%M:%S", cur_time);
+    return time_buf;
+}
+
 /**
  * Find execuatable file from PATH.
  */
-char* find_file_from_path(char* filename) {
-  char* path = getenv("PATH");
+char* find_file_from_path(char* filename, tok_t path_tokens[]) {
   char* ret = (char*) malloc(PATH_MAX + MAXLINE + 2);
-  tok_t* path_tokens;
   struct dirent* ent;
-  if (path != NULL) {
-    path_tokens = get_toks(path);
-    for (int i = 1; i < MAXTOKS && path_tokens[i]; ++i) {
-      DIR* dir;
-      if ((dir = opendir(path_tokens[i])) == NULL) continue;
-      while ((ent = readdir(dir)) != NULL) {
-        if (strcmp(ent->d_name, filename) == 0) {
-          strncpy(ret, path_tokens[i], PATH_MAX);
-          strncat(ret, "/", 1);
-          strncat(ret, filename, MAXLINE);
-          return ret;
-        }
+  for (int i = 1; i < MAXTOKS && path_tokens[i]; ++i) {
+    DIR* dir;
+    if ((dir = opendir(path_tokens[i])) == NULL)
+      continue;
+    while ((ent = readdir(dir)) != NULL) {
+      if (strcmp(ent->d_name, filename) == 0) {
+        strncpy(ret, path_tokens[i], PATH_MAX);
+        strncat(ret, "/", 1);
+        strncat(ret, filename, MAXLINE);
+        return ret;
       }
-      closedir(dir);
     }
+    closedir(dir);
   }
   return NULL;
 }
 
 /**
- * Exec process
+ * IO Redirection
  */
-void exec_process(tok_t arg[]) {
-  char* eval = find_file_from_path(arg[0]);
+int io_redirect(tok_t arg[]) {
+  int i, fd;
+  int in_redir = is_direct_tok(arg, "<");
+  int out_redir = is_direct_tok(arg, ">");
+  if (in_redir != 0) {
+    if (arg[in_redir + 1] == NULL ||
+        strcmp(arg[in_redir + 1], ">") == 0 ||
+        strcmp(arg[in_redir + 1], "<") == 0)
+    {
+      fprintf(stderr, "%s : Syntax error.\n", arg[0]);
+      return -1;
+    }
+    if ((fd = open(arg[in_redir + 1], O_RDONLY, 0)) < 0) {
+      fprintf(stderr, "%s : No such file or directory.\n", arg[in_redir + 1]);
+      return -1;
+    }
+    dup2(fd, STDIN_FILENO);
+    for (i = in_redir; i < MAXTOKS - 2 && arg[i + 2]; ++i) {
+      arg[i] = arg[i + 2];
+    }
+    arg[i] = NULL;
+  }
+  if (out_redir != 0) {
+    if (arg[out_redir + 1] == NULL ||
+        strcmp(arg[out_redir + 1], ">") == 0 ||
+        strcmp(arg[out_redir + 1], "<") == 0)
+    {
+      fprintf(stderr, "%s : Syntax error.\n", arg[0]);
+      return -1;
+    }
+    if ((fd = open(arg[out_redir + 1], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)) < 0) {
+      fprintf(stderr, "%s : No such file or directory.\n", arg[out_redir + 1]);
+      return -1;
+    }
+    dup2(fd, STDOUT_FILENO);
+    for (i = out_redir; i < MAXTOKS - 2 && arg[i + 2]; ++i) {
+      arg[i] = arg[i + 2];
+    }
+    arg[i] = NULL;
+  }
+  return 0;
+}
+
+/**
+ * Path resolve
+ */
+void path_resolve(tok_t arg[], tok_t path_tokens[]) {
+  char* eval = find_file_from_path(arg[0], path_tokens);
   if (eval != NULL) {
     arg[0] = eval;
   }
@@ -169,14 +229,21 @@ void init_shell() {
 int shell(int argc, char *argv[]) {
   char *input_bytes;
   tok_t *tokens;
-  int line_num = 0;
+  tok_t *path_tokens;
   int fundex = -1;
+  char* path = (char*) malloc(PATH_MAX + 1);
+
+  /* copy a new path var to use */
+  strncpy(path, getenv("PATH"), PATH_MAX);
+  path_tokens = get_toks(path);
+
 
   init_shell();
 
-  if (shell_is_interactive)
+  if (shell_is_interactive) {
     /* Please only print shell prompts when standard input is not a tty */
-    fprintf(stdout, "%d: ", line_num);
+    fprintf(stdout, "\033[;34mrayn\33[0m [\033[;32m%s\33[0m] \033[;31m%s\33[0m > ", get_current_time(), getcwd(NULL, 0));
+  }
 
   while ((input_bytes = freadln(stdin))) {
     tokens = get_toks(input_bytes);
@@ -187,7 +254,10 @@ int shell(int argc, char *argv[]) {
       /* REPLACE this to run commands as programs. */
       pid_t pid = fork();
       if (pid == 0) {
-        exec_process(tokens);
+        if (io_redirect(tokens) < 0) {
+          exit(0);
+        }
+        path_resolve(tokens, path_tokens);
         if (execv(tokens[0], tokens) < 0) {
           fprintf(stderr, "%s : Command not found\n", tokens[0]);
           exit(0);
@@ -202,7 +272,7 @@ int shell(int argc, char *argv[]) {
 
     if (shell_is_interactive)
       /* Please only print shell prompts when standard input is not a tty */
-      fprintf(stdout, "%d: ", ++line_num);
+      fprintf(stdout, "\033[;34mrayn\33[0m [\033[;32m%s\33[0m] \033[;31m%s\33[0m > ", get_current_time(), getcwd(NULL, 0));
   }
 
   return 0;
